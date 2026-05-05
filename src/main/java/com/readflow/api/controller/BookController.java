2cf43749f307fb893a2993042f3695d0dae220e9
Matheus Melo <matheuslmelo.dev@gmail.com>
2026-05-05 18:50:10 -0300

    add logs to service and controller

--- src/main/java/com/readflow/api/controller/BookController.java
+++ src/main/java/com/readflow/api/controller/BookController.java
@@ -7,6 +7,8 @@ import com.readflow.api.dto.mapper.BookMapper;
 import com.readflow.api.entity.Book;
 import com.readflow.api.service.BookService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +18,8 @@ import java.util.List;
 @RequestMapping("/books")
 public class BookController {
 
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
+
     private final BookService bookService;
 
     public BookController(BookService bookService) {
@@ -24,6 +28,7 @@ public class BookController {
 
     @GetMapping("/{id}")
     public ResponseEntity<BookResponseDTO> getById(@PathVariable Long id) {
+        log.debug("GET /books/{}", id);
         Book book = bookService.findById(id);
         return ResponseEntity.ok(BookMapper.toResponse(book));
     }
@@ -32,8 +37,10 @@ public class BookController {
     public ResponseEntity<List<BookResponseDTO>> getBooks(@RequestParam(required = false) String title) {
         List<Book> books = null;
         if (title != null) {
+            log.debug("GET /books?title={}", title);
             books = bookService.findByTitle(title);
         }else{
+            log.debug("GET /books");
             books = bookService.findAll();
         }
         return ResponseEntity.ok(BookMapper.toResponseList(books));
@@ -41,18 +48,21 @@ public class BookController {
 
     @PostMapping
     public ResponseEntity<BookResponseDTO> create(@Valid @RequestBody BookCreateDTO requestDTO) {
+        log.debug("POST /books", requestDTO.getTitle());
         Book savedBook = bookService.create(requestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(BookMapper.toResponse(savedBook));
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<BookResponseDTO> update(@PathVariable Long id, @RequestBody BookUpdateDTO requestDTO){
+        log.debug("PATCH /books/{}", id);
         Book book = bookService.update(id,requestDTO);
         return ResponseEntity.ok(BookMapper.toResponse(book));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
+        log.debug("DELETE /books/{}", id);
         bookService.delete(id);
         return ResponseEntity.noContent().build();
     }
--- src/main/java/com/readflow/api/service/BookService.java
+++ src/main/java/com/readflow/api/service/BookService.java
@@ -7,7 +7,8 @@ import com.readflow.api.entity.Book;
 import com.readflow.api.exception.global.exceptions.BookNotFoundException;
 import com.readflow.api.repository.BookRepository;
 import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class BookService {
 
     private final BookRepository bookRepository;
 
+    private static final Logger log = LoggerFactory.getLogger(BookService.class);
 
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
@@ -24,32 +26,46 @@ public class BookService {
 
     public Book findById(Long id) {
         return bookRepository.findById(id)
-                .orElseThrow(() -> new BookNotFoundException("Book not found with id: "+id));
+                .orElseThrow(() -> {
+                    log.warn("Book not found with id: {}", id);
+                    return new BookNotFoundException("Book not found with id: " + id);
+                });
     }
 
     public List<Book> findByTitle(String title){
+        log.debug("Searching books with title: {}", title != null ? title : "ALL");
         return bookRepository.findByTitleContainingIgnoreCase(title);
     }
 
     public List<Book> findAll(){
+        log.debug("Fetching all books");
         return bookRepository.findAll();
     }
 
-    public Book create(@Valid BookCreateDTO requestDTO) {
+    public Book create(BookCreateDTO requestDTO) {
+        log.info("Creating new book with title: {}", requestDTO.getTitle());
         return bookRepository.save(BookMapper.toEntity(requestDTO));
     }
 
     @Transactional
     public Book update(Long id, BookUpdateDTO requestDTO) {
         Book existing = bookRepository.findById(id)
-                .orElseThrow(() -> new BookNotFoundException("Book not found with id: "+id));
+                .orElseThrow(() -> {
+                    log.warn("Book not found with id: {}", id);
+                    return new BookNotFoundException("Book not found with id: " + id);
+                });
+        log.info("Updating book with id: {}", id);
         BookMapper.updateEntity(existing,requestDTO);
         return existing;
     }
 
     public void delete(Long id){
         Book existing = bookRepository.findById(id)
-                .orElseThrow(() -> new BookNotFoundException("Book not found with id: " + id));
-        bookRepository.deleteById(id);
+                .orElseThrow(() -> {
+                    log.warn("Book not found with id: {}", id);
+                    return new BookNotFoundException("Book not found with id: " + id);
+                });
+        log.info("Deleting book with id: {}", id);
+        bookRepository.delete(existing);
     }
 }
